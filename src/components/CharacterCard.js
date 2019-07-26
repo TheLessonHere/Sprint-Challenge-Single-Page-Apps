04d64d21d02cfd14a12176dd4433a1e80f95bdff
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-07-26 10:10:27 -0600

    CharacterList component functioning

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import { Icon } from 'semantic-ui-react';
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard (props) {
+  const { img, name, species, status, location, origin } = props;
+
+  return (
+    <div>
+      <img src={img} alt='character image' />
+      <h2>{name}</h2>
+      <span>{species}</span>{" "}
+      <span>{status}</span>
+      <p>Location: {location}</p>
+      <p>Origin: {origin}</p>
+      <p><Icon name='user' />Episodes</p>
+    </div>
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([{"id":1,"name":"Rick Sanchez","status":"Alive","species":"Human","type":"","gender":"Male","origin":{"name":"Earth (C-137)","url":"https://rickandmortyapi.com/api/location/1"},"location":{"name":"Earth (Replacement Dimension)","url":"https://rickandmortyapi.com/api/location/20"},"image":"https://rickandmortyapi.com/api/character/avatar/1.jpeg"},
+{"id":2,"name":"Morty Smith","status":"Alive","species":"Human","type":"","gender":"Male","origin":{"name":"Earth (C-137)","url":"https://rickandmortyapi.com/api/location/1"},"location":{"name":"Earth (Replacement Dimension)","url":"https://rickandmortyapi.com/api/location/20"},"image":"https://rickandmortyapi.com/api/character/avatar/2.jpeg"}])
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios.get('https://rickandmortyapi.com/api/character/')
+    .then((res) => {
+      setCharacters(res.data.results);  
+    })
+    .catch((err) => {
+      console.log('Character Data Not Available')
+    })
   }, [])
 
-  return <section className='character-list grid-view'>
+  return (
+  
+  <section className='character-list grid-view'>
 
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    {characters.map((character => {
+      return <CharacterCard img={character.image} 
+      name={character.name} 
+      species={character.species}
+      status={character.status}
+      location={character.location.name}
+      origin= {character.origin.name} />
+    }))}
 
+  </section>
+  )
 }
